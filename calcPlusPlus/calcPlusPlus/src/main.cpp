162d898409f952d3f076af392781d852677056ef
chriss0612 <chrisi.schrefl@gmail.com>
2018-05-31 23:12:21 +0200

    Completed the command parser

--- calcPlusPlus/calcPlusPlus/src/main.cpp
+++ calcPlusPlus/calcPlusPlus/src/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 void main(char argc, char **argv)
 {
-	string flags(), arg();
-	system("pause");
+	string flags, ParserArg;
+
+	//Inital syntax check
 	if (argc < 2)
 	{
 		cout << "Expected at least one argument" << endl;
@@ -14,13 +15,33 @@ void main(char argc, char **argv)
 		cout << "For Help type: " << argv[0] << " -h" << endl;
 	}
 
-	system("pause");
-	for (int i = 1; i < argc; i++)
+	//Parsing arguments to flags
+	int flagEnd = argc - 1;
+	for (int i = 1; i < argc -1; i++) //argc -1 so that the last argument will always be directly sent to the parser
 	{
-		if (argv[i][0])
+		if (argv[i][0] == '-')
 		{
-
+			int pos = 1;
+			while (argv[i][pos])
+			{
+				flags += argv[i][pos];
+				pos++;
+			}
+		}
+		else // íf an argument isnt a flag everything after will also not be a flag 
+		{
+			flagEnd = i;
+			break;
 		}
 	}
+	/// **TODO Flag Handeling / Checking**
 
+	//Constructing the string for the parser
+	for (int i = flagEnd; i < argc; i++)
+	{
+		ParserArg += argv[i];
+	}
+
+	/// **TODO Send to parser**
+	/// **TODO output**
 }
