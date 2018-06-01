6a5e87303c36f772879488f8e5280bae5eb4671d
chriss0612 <chrisi.schrefl@gmail.com>
2018-06-01 19:29:47 +0200

    Wrote Parser wrapper

--- calcPlusPlus/calcPlusPlus/src/calcParser.cpp
+++ calcPlusPlus/calcPlusPlus/src/calcParser.cpp
@@ -1,6 +1,22 @@
 #include "calcParser.h"
+#include <string.h>
 
-void parse(string flags, char* unparsed)
+void parse(string flags, string unparsed) //Wrapper arround the recursiv parser
 {
+	char* unparsedCstr = new char[unparsed.length() + 1];
+	char* flagsCstr = new char[flags.length() + 1];
 
+	strcpy(unparsedCstr, unparsed.c_str);
+	strcpy(flagsCstr, flags.c_str);
+
+	parseRec(unparsedCstr, flagsCstr, 0, unparsed.length());
+
+	//Cleanups
+	delete[] unparsedCstr;
+	delete[] flagsCstr;
+}
+
+void parseRec(char* unparsed, char* flags, uint32_t start, uint32_t end)
+{
+	/// ** TODO Write Parser **
 }
