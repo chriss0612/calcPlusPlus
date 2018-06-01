5394cdaa01de9e581d1cb08a66024fccadb84d33
StefanTieber <38786543+StefanTieber@users.noreply.github.com>
2018-06-01 20:57:04 +0200

    clean up

--- calcPlusPlus/calcPlusPlus/src/mathExpression.h
+++ calcPlusPlus/calcPlusPlus/src/mathExpression.h
@@ -20,26 +20,31 @@ class mathExpression
 
 class ZeroMemberOperation : public mathExpression
 {
-	virtual number calculate() = 0;
+    public:
+        virtual number calculate() = 0;
 };
 
-class numberExpression : public ZeroMemberOperation
-{
-    number value;
-
-	virtual number calculate() = 0;
-};
 class OneMemberOperation : public mathExpression
 {
-	mathExpression* mathexpression;
+    public:
+        mathExpression* mathexpression;
 
-	virtual number calculate() = 0;
+        virtual number calculate() = 0;
 };
 
 class TwoMemberOperation : public mathExpression
 {
-	mathExpression* mathexpression1;
-	mathExpression* mathexpression2;
+    public:
+        mathExpression* mathexpression1;
+        mathExpression* mathexpression2;
 
-	virtual number calculate() = 0;
+        virtual number calculate() = 0;
+};
+
+class numberExpression : public ZeroMemberOperation
+{
+    public:
+        number value;
+
+        virtual number calculate() = 0;
 };
