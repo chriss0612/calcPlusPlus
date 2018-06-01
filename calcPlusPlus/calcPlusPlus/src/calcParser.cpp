c28d0437ab1b14025482c7bf73c9f9520446f6b7
chriss0612 <chrisi.schrefl@gmail.com>
2018-06-01 19:37:54 +0200

    Enhanced Parser Wrapper

--- calcPlusPlus/calcPlusPlus/src/calcParser.cpp
+++ calcPlusPlus/calcPlusPlus/src/calcParser.cpp
@@ -1,13 +1,16 @@
 #include "calcParser.h"
 #include <string.h>
 
+void parseRec(char* unparsed, char* flags, uint32_t start, uint32_t end);
+
 void parse(string flags, string unparsed) //Wrapper arround the recursiv parser
 {
 	char* unparsedCstr = new char[unparsed.length() + 1];
 	char* flagsCstr = new char[flags.length() + 1];
 
-	strcpy(unparsedCstr, unparsed.c_str);
-	strcpy(flagsCstr, flags.c_str);
+	memcpy(unparsedCstr, unparsed.c_str(), unparsed.length());
+	memcpy(flagsCstr, flags.c_str() , flags.length());
+
 
 	parseRec(unparsedCstr, flagsCstr, 0, unparsed.length());
 
