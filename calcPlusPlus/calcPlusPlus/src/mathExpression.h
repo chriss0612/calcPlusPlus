932c5b62f04b9bffe393819edd533095993f8a40
chriss0612 <chrisi.schrefl@gmail.com>
2018-06-01 20:58:27 +0200

    Added additon

--- calcPlusPlus/calcPlusPlus/src/mathExpression.h
+++ calcPlusPlus/calcPlusPlus/src/mathExpression.h
@@ -3,7 +3,7 @@
 
 class number
 {
-
+	friend number operator+(number, number);
 };
 
 class integer : public number
@@ -23,23 +23,37 @@ class ZeroMemberOperation : public mathExpression
 	virtual number calculate() = 0;
 };
 
-class numberExpression : public ZeroMemberOperation
-{
-    number value;
-
-	virtual number calculate() = 0;
-};
 class OneMemberOperation : public mathExpression
 {
+protected:
 	mathExpression* mathexpression;
-
+private:
 	virtual number calculate() = 0;
 };
 
 class TwoMemberOperation : public mathExpression
 {
+protected:
 	mathExpression* mathexpression1;
 	mathExpression* mathexpression2;
+public:
+	virtual number calculate() = 0;
+};
+
+
+
+class numberExpression : public ZeroMemberOperation
+{
+	number value;
 
 	virtual number calculate() = 0;
+};
+
+class Addition : public TwoMemberOperation
+{
+
+	virtual number calculate()
+	{
+		return mathexpression1->calculate() + mathexpression2->calculate();
+	}
 };
