9fbc56c2031707e13b4097d4ddbb65f4b20f112d
chriss0612 <chrisi.schrefl@gmail.com>
2018-06-08 16:22:39 +0200

    Programmed and Tested a big part of the Calculation code

--- calcPlusPlus/calcPlusPlus/src/mathExpression.cpp
+++ calcPlusPlus/calcPlusPlus/src/mathExpression.cpp
@@ -1,6 +1,73 @@
 #include "mathExpression.h"
 
-number numberExpression::calculate()
+#define OPER(OP) \
+if (a.isInt())	\
+{	\
+	if (b.isInt()) \
+		return *a.pint OP *b.pint; \
+	if (b.isFloat()) \
+		return *a.pint OP *b.pfloat; \
+	if (b.isCompex()) \
+		return *a.pint OP *b.pcomplex; \
+} \
+if (a.isFloat()) \
+{ \
+	if (b.isInt()) \
+		return *a.pfloat OP *b.pint; \
+	if (b.isFloat()) \
+		return *a.pfloat OP *b.pfloat; \
+	if (b.isCompex()) \
+		return *a.pfloat OP *b.pcomplex; \
+} \
+if (a.isInt()) \
+{ \
+	if (b.isInt()) \
+		return *a.pcomplex OP *b.pint; \
+	if (b.isFloat()) \
+		return *a.pcomplex OP *b.pfloat; \
+	if (b.isCompex()) \
+		return *a.pcomplex OP *b.pcomplex; \
+}
+
+
+number operator+(number a , number b)
 {
-    return value;
+	OPER(+)
+}
+
+number operator-(number a, number b)
+{
+	OPER(-)
+}
+
+number operator*(number a, number b)
+{
+	OPER(*)
+}
+
+number operator/(number a, number b)
+{
+	OPER(/);
+}
+
+number::~number()
+{
+	if (isInt())
+		delete pint;
+	if (isFloat())
+		delete pfloat;
+	if (isCompex())
+		delete pcomplex;
+}
+
+string number::toString()
+{
+	if (isInt())
+		return std::to_string(*pint);
+	if (isFloat())
+		return std::to_string(*pfloat);
+	if (isCompex())
+		return "" + std::to_string(pcomplex->real()) + " + " + std::to_string(pcomplex->imag()) + "j";
+
+	return "Something went wrong";
 }
--- calcPlusPlus/calcPlusPlus/src/mathExpression.h
+++ calcPlusPlus/calcPlusPlus/src/mathExpression.h
@@ -1,19 +1,65 @@
 #pragma once
+#include "complex.h"
+#include <string>
 
 
+// This should definitly be done better but i dont know how
 class number
 {
+private:
+	int *pint;
+	float *pfloat;
+	complex *pcomplex;
 public:
-	friend number operator+(number, number);  /// **TODO Add these funktions **
+	number()			{pint = nullptr;	pfloat = nullptr;		pcomplex = nullptr; }
+	number(int x)		{pint = new int(x);	pfloat = nullptr;		pcomplex = nullptr;}
+	number(float x)		{pint = nullptr;	pfloat = new float(x);	pcomplex = nullptr;}
+	number(complex x)	{pint = nullptr;	pfloat = nullptr;		pcomplex = new complex(x);}
+	
+	
+	number(const number& n) {
+		if (n.isInt())
+			pint = new int(*n.pint);
+		else
+			pint = nullptr;
+		if (n.isFloat())
+			pfloat = new float(*n.pfloat);
+		else
+			pfloat = nullptr;
+		if (n.isCompex())
+			pcomplex = new complex(*n.pcomplex);
+		else
+			pcomplex = nullptr;
+	}
+	
+	void operator=(number n)
+	{
+		if (n.isInt())
+			pint = new int(*n.pint);
+		else
+			pint = nullptr;
+		if (n.isFloat())
+			pfloat = new float(*n.pfloat);
+		else
+			pfloat = nullptr;
+		if (n.isCompex())
+			pcomplex = new complex(*n.pcomplex);
+		else
+			pcomplex = nullptr;
+	}
+	
+	~number();// Making sure to clean up aftr myself
+
+	string toString();
+	
+	friend number operator+(number, number);
 	friend number operator-(number, number);
 	friend number operator*(number, number);
 	friend number operator/(number, number);
-};
 
-class integer : public number
-{
-public:
-    int value;
+	bool isInt() const { return pint != nullptr; }
+	bool isFloat() const { return pfloat != nullptr; }
+	bool isCompex() const { return pcomplex != nullptr; }
 };
 
 class mathExpression
@@ -46,25 +92,14 @@ public:
 
 class numberExpression : public ZeroMemberOperation
 {
-protected:
-	mathExpression* mathexpression1;
-	mathExpression* mathexpression2;
-public:
-	virtual number calculate() = 0;
-};
-
-
-
-class numberExpression : public ZeroMemberOperation
-{
 	number value;
-
-	virtual number calculate() = 0;
+public:
+	virtual number calculate() { return value; };
 };
 
 class Addition : public TwoMemberOperation
 {
-
+public:
 	virtual number calculate()
 	{
 		return mathexpression1->calculate() + mathexpression2->calculate();
@@ -72,7 +107,7 @@ class Addition : public TwoMemberOperation
 };
 class Subtraction : public TwoMemberOperation
 {
-
+public:
 	virtual number calculate()
 	{
 		return mathexpression1->calculate() - mathexpression2->calculate();
@@ -81,7 +116,7 @@ class Subtraction : public TwoMemberOperation
 
 class Multiplication : public TwoMemberOperation
 {
-
+public:
 	virtual number calculate()
 	{
 		return mathexpression1->calculate() * mathexpression2->calculate();
@@ -90,7 +125,7 @@ class Multiplication : public TwoMemberOperation
 
 class Division : public TwoMemberOperation
 {
-
+public:
 	virtual number calculate()
 	{
 		return mathexpression1->calculate() / mathexpression2->calculate();
